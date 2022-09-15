09f6ea586afc459dc306476f9458970bca949b56
938938 <938kjh@gmail.com>
2022-09-15 23:54:57 +0900

    ::State 및 tYPEsCRIPT 관리하기

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-const NewTodo = () => {
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+  // 함수 반환 타입 : void
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
   const submitHandler = (e: React.FormEvent) => {
     e.preventDefault();
+    const enteredText = todoTextInputRef.current!.value;
+    if (enteredText.trim().length === 0) {
+      // 입력된 값이 없을 경우
+      return;
+    }
+    props.onAddTodo(enteredText);
   };
+
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor='text'>Todo Text</label>
-      <input type='text' id='text' />
+      <input type='text' id='text' ref={todoTextInputRef} />
       <button>Add Todo</button>
     </form>
   );
